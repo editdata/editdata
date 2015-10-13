0ffe7a3e740c397e48f8a34d9364df450ea7fb33
sethvincent <sethvincent@gmail.com>
2015-10-13 11:44:56 -0700

    simplify, create lib/ui.js file

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var fs = require('fs')
-var os = require('os')
-var path = require('path')
 var ipc = require('ipc')
-var mkdirp = require('mkdirp')
 var request = require('xhr')
 var h = require('virtual-dom/h')
 var DataEditor = require('data-editor')
-
 var createStore = require('redux').createStore
 var minimist = require('minimist')
+var createUI = require('./lib/ui')
 
 var views = {
   grid: require('data-grid')(),
@@ -40,71 +37,41 @@ views.grid.addEventListener('blur', function (e, row, key, value) {})
 views.grid.addEventListener('input', function (e, row, key, value) {})
 
 ipc.on('args', function (args) {
-  store.dispatch({
-    type: 'args',
-    args: minimist(args)
+  store.dispatch({ type: 'args', args: minimist(args) })
+  getData(function (err, data) {
+    if (err) console.log(err)
+    store.dispatch({ type: 'format', data: data })
+    render(store.getState())
   })
-  ready()
 })
 
 var appEl = document.getElementById('app')
 var editor = DataEditor(appEl, {})
 
-function ready () {
-  var dir = path.join(os.homedir(), '.editdata')
-  mkdirp(dir, function (err) {
-    if (err) console.log(err)
-
-    getData(function (err, res) {
-      if (err) console.log(err)
-      store.dispatch({
-        type: 'format',
-        data: res
-      })
-    })
-  })
-}
-
 function getData (callback) {
   var state = store.getState()
+
   if (state.args._[0]) {
     fs.readFile(state.args._[0], 'utf8', function (err, data) {
-      if (err) return callback(err)
-      callback(null, JSON.parse(data))
+      returnData(err, data)
     })
   } else if (state.args.url) {
-    request(state.args.url, function (err, res, body) {
-      if (err) return callback(err)
-      callback(null, JSON.parse(body))
+    request(state.args.url, function (err, res, data) {
+      returnData(err, data)
     })
+  }
+
+  function returnData (err, data) {
+    if (err) return callback(err)
+    data = JSON.parse(data)
+    callback(null, data)
   }
 }
 
 function render (state) {
-  if (state) {
-    var view = views[state.view].render(state)
-    var ui = createUI(state)
-    editor.render([ui, h('div.view-wrapper', [view])], state)
-  }
+  var view = views[state.view].render(state)
+  var ui = createUI(state, views)
+  editor.render([ui, h('div.view-wrapper', [view])], state)
 }
 
-function createUI (state) {
-  var viewButtons = []
-  Object.keys(views).forEach(function (key) {
-    viewButtons.push(h('button#.view-choice', {
-      onclick: function (e) {
-        e.preventDefault()
-        if (state.view !== key) {
-          store.dispatch({
-            type: 'set_view',
-            view: key
-          })
-        }
-      }
-    }, key))
-  })
-
-  return h('div.editor-ui', [
-    h('div.editor-header', viewButtons)
-  ])
-}
+render(store.getState())
